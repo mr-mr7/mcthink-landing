f6643642b809003dcb8008d9d7d7bd798d9a6493
mohamadddd1998 <mohamadmirzaei1998@gmail.com>
2024-06-28 20:35:05 +0330

    commit

--- src/service/index.js
+++ src/service/index.js
@@ -1,20 +1,31 @@
 import { Api } from "@/api/config";
+import axios from "axios";
 
 class services {
+  // getPageData = async (endpoint = "", params = {}) => {
+  //   try {
+  //     const res = await fetch(
+  //       Api.baseUrl + endpoint + `?${new URLSearchParams(params).toString()}`,
+  //       {
+  //         method: "get",
+  //         cache: "no-store",
+  //       }
+  //     );
+  //     if (!res.ok) {
+  //       throw new Error();
+  //     } else {
+  //       return res.json();
+  //     }
+  //   } catch (error) {
+  //     throw new Error();
+  //   }
+  // };
   getPageData = async (endpoint = "", params = {}) => {
     try {
-      const res = await fetch(
-        Api.baseUrl + endpoint + `?${new URLSearchParams(params).toString()}`,
-        {
-          method: "get",
-          cache: "no-store",
-        }
-      );
-      if (!res.ok) {
-        throw new Error();
-      } else {
-        return res.json();
-      }
+      const res = await axios.get(Api.baseUrl + endpoint, {
+        params,
+      });
+      return res.data
     } catch (error) {
       throw new Error();
     }
