2603ecf023934034d5eeae97f1bbdf3b993299d9
mohamadddd1998 <mohamadmirzaei1998@gmail.com>
2024-06-28 15:27:59 +0330

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import MainProvider from "@/main";
-//import ProgressBar from "@/structure/organism/PrgressBar";
+import ProgressBar from "@/structure/organism/PrgressBar";
 
 export const metadata = {
   title: "سایت خبری",
@@ -13,12 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa">
       <body className="body-inner">
-        {/* <ProgressBar
+        <ProgressBar
           height="4px"
           color="#1e88e5"
           options={{ showSpinner: true }}
           shallowRouting
-        /> */}
+        />
         <MainProvider>{children}</MainProvider>
       </body>
     </html>
